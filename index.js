4d9ec6b7bca593068ecd70fdfb00b2ae2016f8ce
faddy1184 <136419272+faddy1184@users.noreply.github.com>
2024-05-11 15:53:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+var typed = new Typed('#element', {
+    strings: ['HTML developer.', 'CSS Developer.', 'JS Developer.', 'More to come'],
+    typeSpeed: 50,
+});
+
+function redirectTolink() {
+    window.open('https://github.com/faddy1184');
+}
+function viewResume() {
+    window.open(`./images/faddy'sresume.pdf`, `_blank`);
+}
+function insta() {
+    window.open(`https://www.instagram.com/faddy._.1184/`);
+}
+function watsapp() {
+    window.open(`https://wa.link/71wnsc`);
+}
+function project() {
+    window.open(` https://faddy1184.github.io/i-educate/`);
+}
+function reloadpage(){
+    window.location.reload();
+}
